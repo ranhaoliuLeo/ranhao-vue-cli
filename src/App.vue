85db1538804780ada58c486cf329cc530df9062b
Leo <leoisbeste@qq.com>
2020-12-18 15:03:15 +0800

    现在可以使用基础的vue程序了，缺省router，vuex，以及一些不可或缺的插件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div>
+        <h2>Welcom to the ranhao CLI! </h2>
+        <p>now is {{ data }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      data: 'Wow!',
+      timer: ''
     };
   },
-
-  methods: {}
+  created() {
+    const that = this
+    that.timer = setInterval(() => {
+      console.log(11)
+      that.data = Date()
+    }, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
 }
 
 </script>
 <style scoped>
-
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 95vh;
+}
+.container h2 {
+  text-align: center;
+  font-size: 2.8vw;
+}
+.container p {
+  text-align: center;
+  font-size: 1vw;
+  color: #aaaaaa;
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,63 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     entry: {
         main: './src/main.js'
     },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.runtime.esm.js'
+        }
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename:'bundle.js'
+        filename:'bundle.js',
+        publicPath: '',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            },
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: 'vue-loader'
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './assets/index.html')
-        })
+            template: path.resolve(__dirname, './src/assets/index.html')
+        }),
+        new VueLoaderPlugin()
     ],
     devServer: {
         // https: true,
         open: true,
-        host: '0,0,0,0',
         port: 8000,
-        dis
+        disableHostCheck: true,
+        hot: true,
+        // proxy: {}
     }
 }
